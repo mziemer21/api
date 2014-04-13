d0139dadbbb8570f101ea066bbd482a41720eb7d
Prelang Builder <builder@prelang.com>
2014-04-13 14:55:38 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   has_many :establishments
   has_many :rewiews
   has_many :images
+  belongs_to :location
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
