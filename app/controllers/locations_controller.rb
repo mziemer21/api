04e38032348d11dc065922193cb2c9d8f3598561
Prelang Builder <builder@prelang.com>
2014-04-13 14:56:05 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,8 @@
 class LocationsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   # GET /locations
